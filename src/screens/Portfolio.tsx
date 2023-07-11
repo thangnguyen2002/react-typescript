1a36b89e1e0f1dcc35e3f93278df07593a193765
a41220 <ducthang642002@gmail.com>
2023-07-11 18:41:28 +0700

    ud

---
+++ src/screens/Portfolio.tsx
@@ -0,0 +1,47 @@
+import LeftPart from "../components/LeftPart";
+import RightPart from "../components/RightPart";
+import MobileMenu from "../components/mobile/MobileMenu";
+import Preloader from "../components/Preloader";
+
+const Portfolio = () => {
+    return (
+        <>
+        {/* WRAPPER ALL */}
+        <div className="arlo_tm_wrapper_all">
+
+            <div id="arlo_tm_popup_blog">
+                <div className="container">
+                    <div className="inner_popup scrollable"></div>
+                </div>
+                <span className="close"><a href="#"></a></span>
+            </div>
+            
+            {/* PRELOADER */}
+            <Preloader/>
+            {/* /PRELOADER */}
+            
+            {/* MOBILE MENU */}
+            <MobileMenu/>
+            {/* /MOBILE MENU */}
+            
+            {/* CONTENT */}
+            <div className="arlo_tm_content">
+                
+                {/* LEFTPART */}
+                <LeftPart/>
+                {/* /LEFTPART */}
+                
+                {/* RIGHTPART */}
+                <RightPart/>
+                {/* /RIGHTPART */}
+                
+                <a className="arlo_tm_totop" href="#"></a> 
+                
+            </div>
+        </div>
+        {/* / WRAPPER ALL */}
+        </>
+     );
+}
+ 
+export default Portfolio;
